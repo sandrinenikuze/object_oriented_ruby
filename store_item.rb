ebc774105095a71e69b1086e099c62d12c1e703d
Sandrine Nikuze <nikuzesandrine@yahoo.fr>
2017-01-11 14:26:56 +0200

    Items in my store

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+itme1 = {name: "watch", color: "red", price: "5000"}
+item2 = {"name" => "laptop", "color" => "black", "price" => "500000"}
+item3 = {name: "Telephone", color: "blue", price: "50000"}
