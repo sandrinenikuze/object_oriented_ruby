a781b0d2c5baf6ce74dc7bbd53371f1746b4de83
Sandrine Nikuze <nikuzesandrine@yahoo.fr>
2017-01-12 12:00:38 +0200

    refuctoring electronics file

--- electronics.rb
+++ electronics.rb
@@ -1,30 +1,29 @@
 class Electronics 
 
+  attr_reader :name, :color, :price 
+  attr_writer :price
   def initialize (name, color, price)
     @name= name 
     @color= color
     @price= price
   end
 
-  def name 
-    @name 
-  end 
-  def price 
-    @price 
-  end
-  def price= (price)
-    @price = price
-  end 
-  def color
-    @color
-  end 
+  # def name 
+  #   @name 
+  # end 
+  # def price 
+  #   @price 
+  # end
+  # def price= (price)
+  #   @price = price
+  # end 
+  # def color
+  #   @color
+  # end 
   def info
-    "#{Here is the information about the electron} #{name} #{@color} #{@price}"
+    "Here is the information about the electron #{@name} #{@color} #{@price}"
 
   end 
 end
 electronic = Electronics.new("Telephone", "Red", 50000)
-puts electronic.name
-puts electronic. color
-electronic.price = 60000 
-puts electronic.price  
+puts electronic.info 
