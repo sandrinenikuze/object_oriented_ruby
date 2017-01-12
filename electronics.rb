3a6c01a5aa02583dd7b266d87d77d9cc1686c763
Sandrine Nikuze <nikuzesandrine@yahoo.fr>
2017-01-12 11:34:35 +0200

    May first Electronics class

---
+++ electronics.rb
@@ -0,0 +1,30 @@
+class Electronics 
+
+  def initialize (name, color, price)
+    @name= name 
+    @color= color
+    @price= price
+  end
+
+  def name 
+    @name 
+  end 
+  def price 
+    @price 
+  end
+  def price= (price)
+    @price = price
+  end 
+  def color
+    @color
+  end 
+  def info
+    "#{Here is the information about the electron} #{name} #{@color} #{@price}"
+
+  end 
+end
+electronic = Electronics.new("Telephone", "Red", 50000)
+puts electronic.name
+puts electronic. color
+electronic.price = 60000 
+puts electronic.price  
